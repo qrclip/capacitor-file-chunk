e5bc82e93f6b1fdaa6a9d8d607f5af367f1172a1
Jeremy Mouton <jeremy@aspendigital.com>
2024-07-09 10:11:05 -0600

    feat: update plugin exports
    Adds FileChunkServerInfo type to exports.

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,4 @@ import type { FileChunkPlugin } from './definitions';
 
 declare const FileChunk: FileChunkPlugin;
 export * from './definitions';
-export { FileChunk };
+export { FileChunk, FileChunkServerInfo };
